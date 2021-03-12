81adb126c1506cac75a0a7c804f76314aa98006b
Jeff Mendez <jeffmendez19@gmail.com>
2021-03-11 20:59:38 -0500

    chore(mock): add dashboard routing and tab setup

--- app/src/main/java/com/a11y/a11ywatch/MainActivity.kt
+++ app/src/main/java/com/a11y/a11ywatch/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.a11y.a11ywatch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun login(view: View) {
+        setContentView(R.layout.activity_dashboard)
+    }
+    fun register(view: View) {
+        setContentView(R.layout.activity_dashboard)
     }
 }
